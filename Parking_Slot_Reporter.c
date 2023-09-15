7b42562a8319a7ec179cf3bdf381043848b5e875
Glitch-guy0 <prajwalm9845@gmail.com>
2023-09-15 13:21:50 +0530

    working servo
    servo goes from 0 to 90deg when ultrasound.near is true
    and 90deg to 0 if false
    
    warning: not sure about closing thing i couldnot test it properly

--- Parking_Slot_Reporter.c
+++ Parking_Slot_Reporter.c
@@ -1,24 +1,87 @@
+#include <Servo.h>
+
+/******* Test Code ******************************************************************************/
+
+class Ultrasound
+{
+  public:
+    bool near()
+    {
+        return true;
+    }
+};
+Ultrasound ultrasound;
+/*********Test Code end **************************************************************************/
+
 //* Initialization
+
+// creating objects
+Servo gateServo;
+
 // todo: declare pins for each components
 //* led's
-const int master[] = 0;
-const int slots[] = [1,2,3,4];
 
 //* servo motor
-const byte servoPin = A0;
+const int servoPin = 9;
 
 //* IRs at entrance detection
 const int triggerPin = 5;
 const int echoPin = 6;
 
+// initial gate position 
+bool gateOpen = false;
+
+// servo function
+void checkGate(bool isCar)
+{
+    if(isCar && !gateOpen)
+    {
+        int angle = 0;
+        while(angle < 90)
+        {
+            angle += 15;
+            gateServo.write(angle);
+            delay(15);
+            Serial.println(angle); //***** Debug code *****************************************
+        }
+        gateOpen = true;
+    }
+    if(!isCar && gateOpen)
+    {
+        int angle = 90;
+        while(angle < 0)
+        {
+            angle -= 15;
+            gateServo.write(angle);
+            delay(15);
+            Serial.println(angle); //************ Debug code ************************************
+        }
+        gateOpen = false;
+    }
+}
 //* Setup
 void setup()
 {
     // todo: declaring pin mode
+
+/*****  Temporary  ********************************************************************************/
+    // Set the servo pin mode to output
+    pinMode(servoPin, OUTPUT);
+
+    // Set the IR sensor pin modes to input
+    pinMode(triggerPin, INPUT);
+    pinMode(echoPin, INPUT);
+
+    Serial.begin(9600);
+/*****  Temporary  ***********************************************************************************/
+    // * servo pin declaration
+    // Attach the gate servo to pin 9
+    gateServo.attach(servoPin);
+
 }
 
 //* loop
 void loop()
 {
-
+    checkGate(ultrasound.near());
 }
