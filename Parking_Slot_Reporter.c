868af5036b6db9ef58412797913f43e9ff3b5809
Glitch-guy0 <prajwalm9845@gmail.com>
2023-09-15 10:03:48 +0530

    boilerplate and pin declaration

---
+++ Parking_Slot_Reporter.c
@@ -0,0 +1,24 @@
+//* Initialization
+// todo: declare pins for each components
+//* led's
+const int master[] = 0;
+const int slots[] = [1,2,3,4];
+
+//* servo motor
+const byte servoPin = A0;
+
+//* IRs at entrance detection
+const int triggerPin = 5;
+const int echoPin = 6;
+
+//* Setup
+void setup()
+{
+    // todo: declaring pin mode
+}
+
+//* loop
+void loop()
+{
+
+}
