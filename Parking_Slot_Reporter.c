fd2424bf6d55df4a2a4e4139a19a0ab63f54f944
Glitch-guy0 <prajwalm9845@gmail.com>
2023-09-15 16:15:40 +0530

    completed ultrasound sensor class

--- Parking_Slot_Reporter.c
+++ Parking_Slot_Reporter.c
@@ -1,8 +1,6 @@
 //* Initialization
 // todo: declare pins for each components
 //* led's
-const int master[] = 0;
-const int slots[] = [1,2,3,4];
 
 //* servo motor
 const byte servoPin = A0;
@@ -11,6 +9,55 @@ const byte servoPin = A0;
 const int triggerPin = 5;
 const int echoPin = 6;
 
+
+//* ulterasound hos sleep for 100ms
+
+class UltrasoundSensor{
+    private:
+        int signalPin;
+        const int thresholdDistance = 5;
+        long readDistanceCms(int signalPin)
+            {
+                pinMode(signalPin, OUTPUT);  // Clear the trigger
+                digitalWrite(signalPin, LOW);
+                delayMicroseconds(2);
+                // Sets the trigger pin to HIGH state for 10 microseconds
+                digitalWrite(signalPin, HIGH);
+                delayMicroseconds(10);
+                digitalWrite(signalPin, LOW);
+                pinMode(signalPin, INPUT);
+                // Reads the echo pin, and returns the sound wave travel time in cm
+                return 0.01723 * pulseIn(signalPin, HIGH);
+            } 
+
+    public:
+        void UltrasoundSensor(int signalPin)
+        {
+            this.signalPin = signalPin;
+        }
+        bool near()
+        {
+            if(this.thresholdDistance > this.readDistanceCms())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        bool far()
+        {
+            if(this.thresholdDistance <= this.readDistanceCms())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+};
 //* Setup
 void setup()
 {
