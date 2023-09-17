af2b3101f7509b2ad42d873680712dcf29e746d4
Glitch-guy0 <prajwalm9845@gmail.com>
2023-09-17 21:10:04 +0530

    working led indicators

--- Parking_Slot_Reporter.c
+++ Parking_Slot_Reporter.c
@@ -1,24 +1,42 @@
-//* Initialization
-// todo: declare pins for each components
-//* led's
-const int master[] = 0;
-const int slots[] = [1,2,3,4];
+/**
+ * This code sets up an array of infrared sensors and corresponding red and green LEDs, and
+ * continuously checks the sensors to determine if all slots are filled, lighting up the red LED if
+ * they are.
+ */
+ int ir[4] = {6,7,8,9};
+ int ledr[4] = {2,3,4,5};
+ int ledg[4] = {10, 11, 12, 13};
+ int masterRedLedPin = 1;
 
-//* servo motor
-const byte servoPin = A0;
+void setup() {
+  for (int i = 0; i < 4; i++) {
+    pinMode(ir[i], INPUT);
+  }
 
-//* IRs at entrance detection
-const int triggerPin = 5;
-const int echoPin = 6;
-
-//* Setup
-void setup()
-{
-    // todo: declaring pin mode
+  for (int i = 0; i < 4; i++) {
+    pinMode(ledr[i], OUTPUT);
+    pinMode(ledg[i], OUTPUT);
+  }
+  pinMode(masterRedLedPin, OUTPUT);
 }
 
-//* loop
-void loop()
-{
+void loop() {
+  int filledSlots = 0;
 
+  for (int i = 0; i < 4; i++) {
+    if (digitalRead(ir[i]) == HIGH) {
+      digitalWrite(ledr[i], HIGH);
+      digitalWrite(ledg[i], LOW);
+      filledSlots++;
+    } else {
+      digitalWrite(ledr[i], LOW);
+      digitalWrite(ledg[i], HIGH);
+    }
+  }
+
+  if (filledSlots == 4) {
+    digitalWrite(masterRedLedPin, HIGH);
+  } else {
+    digitalWrite(masterRedLedPin, LOW);
+  }
 }
