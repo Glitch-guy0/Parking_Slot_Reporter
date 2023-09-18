27ce97e661f5798ac8ed3a2b065f5203198ac59f
Glitch-guy0 <prajwalm9845@gmail.com>
2023-09-18 18:35:44 +0530

    revised setup & loop, updating gate conditions
    setup and loop functions resolved by making it as one function
    updating gate conditions by checking available slots

--- Parking_Slot_Reporter.c
+++ Parking_Slot_Reporter.c
@@ -1,45 +1,9 @@
-/**
- * This code sets up an array of infrared sensors and corresponding red and green LEDs, and
- * continuously checks the sensors to determine if all slots are filled, lighting up the red LED if
- * they are.
- */
+
  int ir[4] = {6,14,8,15};
  int ledr[4] = {2,3,4,5};
  int ledg[4] = {10, 11, 12, 13};
  int masterRedLedPin = 1;
 
-void setup() {
-  for (int i = 0; i < 4; i++) {
-    pinMode(ir[i], INPUT);
-  }
-
-  for (int i = 0; i < 4; i++) {
-    pinMode(ledr[i], OUTPUT);
-    pinMode(ledg[i], OUTPUT);
-  }
-  pinMode(masterRedLedPin, OUTPUT);
-}
-
-void loop() {
-  int filledSlots = 0;
-
-  for (int i = 0; i < 4; i++) {
-    if (digitalRead(ir[i]) == HIGH) {
-      digitalWrite(ledr[i], HIGH);
-      digitalWrite(ledg[i], LOW);
-      filledSlots++;
-    } else {
-      digitalWrite(ledr[i], LOW);
-      digitalWrite(ledg[i], HIGH);
-    }
-  }
-
-  if (filledSlots == 4) {
-    digitalWrite(masterRedLedPin, HIGH);
-  } else {
-    digitalWrite(masterRedLedPin, LOW);
-  }
-}
 #include <Servo.h>
 
 // declaring servo object
@@ -128,11 +92,40 @@ void setup()
 {
     // Attach the gate servo to pin 9
     gateServo.attach(servoPin);
+      for (int i = 0; i < 4; i++) {
+    pinMode(ir[i], INPUT);
+  }
+
+  for (int i = 0; i < 4; i++) {
+    pinMode(ledr[i], OUTPUT);
+    pinMode(ledg[i], OUTPUT);
+  }
+  pinMode(masterRedLedPin, OUTPUT);
 
 }
 
 //* loop
 void loop()
 {
-    checkGate(ultrasound.near());
+
+      int filledSlots = 0;
+
+  for (int i = 0; i < 4; i++) {
+    if (digitalRead(ir[i]) == HIGH) {
+      digitalWrite(ledr[i], HIGH);
+      digitalWrite(ledg[i], LOW);
+      filledSlots++;
+    } else {
+      digitalWrite(ledr[i], LOW);
+      digitalWrite(ledg[i], HIGH);
+    }
+  }
+
+  if (filledSlots == 4) {
+    digitalWrite(masterRedLedPin, HIGH);
+    checkGate(false); // when no slots dont check for incoming vehicles
+  } else {
+    digitalWrite(masterRedLedPin, LOW);
+        checkGate(ultrasound.near()); // check only when slots are empty
+  }
 }
